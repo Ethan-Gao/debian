46b00fe2304ac4971562938fee427a39ec5effc4
Ethan-Gao <Mr.Ethan.Gao@gmail.com>
2017-04-02 16:32:18 +0800

    Default debian-wheezy rootfs for armhf
    Signed-off-by: Ethan-Gao <Mr.Ethan.Gao@gmail.com>

--- get_rootfs.sh
+++ get_rootfs.sh
@@ -10,7 +10,7 @@ fi
 #set enviroment
 sudo apt-get install qemu-user-static debootstrap binfmt-support
 #download source : xxx-wheezy,jessise,squeeze... yyy-armel,armhf,arm64...
-targetdir=rootfs
-distro=xxx
+targetdir=AM335x
+distro=wheezy
 mkdir $targetdir
-sudo debootstrap --no-check-gpg --arch=yyy --foreign $distro $targetdir
+sudo debootstrap --no-check-gpg --arch=armhf --foreign $distro $targetdir
